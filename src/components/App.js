c1cb4922b5db94346e0fae9fe15b6f00af885cb1
Lenny Peters <mr.lennypeters@gmail.com>
2020-07-20 18:26:08 -0700

    feat: 🎸 Dropdown.js
    updated useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,52 @@
-import React from "react";
-import Accordion from '../components/Accordion';
-import Search from '../components/Search';
+import React, { useState } from "react";
+import Dropdown from "../components/Dropdown";
+import Accordion from "../components/Accordion";
+import Search from "../components/Search";
 
 const items = [
   {
-    title: 'What is React?',
-    description: 'React is a front-end library'
+    title: "What is React?",
+    description: "React is a front-end library",
   },
   {
-    title: 'Why use React?',
-    description: 'React is a favorite js library with engineers'
+    title: "Why use React?",
+    description: "React is a favorite js library with engineers",
   },
   {
-    title: 'How do you use React?',
-    description: 'By creating components'
-  }
-]
+    title: "How do you use React?",
+    description: "By creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Shade of Blue",
+    value: "blue",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [show, setShow] = useState(true);
   return (
     <div>
-      <Search />
+      <button onClick={() => setShow(!show)}>Hide/Show Dropdown</button>
+      {show ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
+      {/* <Search /> */}
       {/* <Accordion items={items}/> */}
     </div>
   );
